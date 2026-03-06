00c3618fcba9398d9f84c664e6eb13cca02afd4a
jayaadityakillamsetty-max <jayaadityakillamsetty@gmail.com>
2026-03-06 21:50:42 +0530

    Update secure-alu_tb.v

--- secure-alu_tb.v
+++ secure-alu_tb.v
@@ -5,16 +5,16 @@ reg reset;
 reg [1:0] key;
 reg [7:0] A;
 reg [7:0] B;
-reg [1:0] op; 
+reg [1:0] op;
 wire [7:0] result;
-  secure_alu uut(
+secure_alu uut(
     .clk(clk),
     .reset(reset),
     .key(key),
     .A(A),
     .B(B),
-   .op(op),
-   .result(result)
+    .op(op),
+    .result(result)
 );
 always #5 clk = ~clk;
 initial
@@ -25,9 +25,11 @@ key = 2'b00;
 A = 8'd10;
 B = 8'd4;
 #10 reset = 0;
+// case 1 wrong attempts
 #10 key = 2'b10;
 #10 key = 2'b11;
 #10 key = 2'b10;
+// case 2 correct sequence 
 #10 key = 2'b01;
 #10 key = 2'b10;
 #10 key = 2'b11;
