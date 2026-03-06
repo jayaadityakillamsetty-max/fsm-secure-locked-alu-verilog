d39b3282da483618273cac0bf9ea71dcc68ac047
jayaadityakillamsetty-max <jayaadityakillamsetty@gmail.com>
2026-03-06 21:33:30 +0530

    Create secure-alu_tb.v

---
+++ secure-alu_tb.v
@@ -0,0 +1,37 @@
+`timescale 1ns/1ps
+module secure_alu_tb;
+reg clk;
+reg reset;
+reg [1:0] key;
+reg [7:0] A;
+reg [7:0] B;
+reg [1:0] op; 
+wire [7:0] result;
+  secure_alu uut(
+    .clk(clk),
+    .reset(reset),
+    .key(key),
+    .A(A),
+    .B(B),
+   .op(op),
+   .result(result)
+);
+always #5 clk = ~clk;
+initial
+begin
+clk = 0;
+reset = 1;
+key = 2'b00;
+A = 8'd10;
+B = 8'd4;
+#10 reset = 0;
+#10 key = 2'b10;
+#10 key = 2'b11;
+#10 key = 2'b10;
+#10 key = 2'b01;
+#10 key = 2'b10;
+#10 key = 2'b11;
+#10 op = 2'b00;
+#50 $finish;
+end
+endmodule
